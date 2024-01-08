6f088a1c25d74b4d3b2b9bee62ee414044dfd87c
Kaukiainen, Sergei <sergei.kaukinen@roimaint.com>
2024-01-08 17:57:45 +0200

    exercise 1.3

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -10,12 +10,12 @@ const Part = ({ part, exercise }) => {
   );
 };
 
-const Content = ({ parts, exercises }) => {
+const Content = ({ parts }) => {
   return (
     <div>
-      <Part part={parts[0]} exercise={exercises[0]} />
-      <Part part={parts[1]} exercise={exercises[1]} />
-      <Part part={parts[2]} exercise={exercises[2]} />
+      {parts.map((part, index) => (
+        <Part key={index} part={part.name} exercise={part.exercises} />
+      ))}
     </div>
   );
 };
@@ -26,21 +26,24 @@ const Total = ({ total }) => {
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
 
   return (
     <div>
       <Header course={course} />
-      <Content
-        parts={[part1, part2, part3]}
-        exercises={[exercises1, exercises2, exercises3]}
-      />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Content parts={[part1, part2, part3]} />
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   );
 };
