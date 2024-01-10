52756ab6bbf9f1e8611d93f30f5b7a34c6ac4201
Kaukiainen, Sergei <sergei.kaukinen@roimaint.com>
2024-01-10 18:55:07 +0200

    exercise 1.8 unicafe

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -1,13 +1,27 @@
 import { useState } from "react";
 
+const Statistic = ({ good, neutral, bad }) => {
+  const sum = () => good + neutral + bad;
+
+  const average = () => (good * 1 + neutral * 0 + bad * -1) / sum();
+
+  return (
+    <>
+      <h2>statistic</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {sum()}</p>
+      <p>average {average() ? average() : 0}</p>
+      <p>positive {sum() ? (good / sum()) * 100 : 0} %</p>
+    </>
+  );
+};
+
 function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-
-  const sum = () => good + neutral + bad;
-
-  const average = () => (good * 1 + neutral * 0 + bad * -1) / sum();
 
   return (
     <div>
@@ -15,13 +29,7 @@ function App() {
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
-      <h2>statistic</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {sum()}</p>
-      <p>average {average()}</p>
-      <p>positive {sum() ? (good / sum()) * 100 : 0} %</p>
+      <Statistic good={good} neutral={neutral} bad={bad} />
     </div>
   );
 }
