6867a3e6514affe0b3eb81979c349e2c19cee1fd
Kaukiainen, Sergei <sergei.kaukinen@roimaint.com>
2024-01-23 18:10:13 +0200

    exercise 2.5 kurssitiedot

--- osa2/kurssitiedot/src/App.jsx
+++ osa2/kurssitiedot/src/App.jsx
@@ -1,43 +1,4 @@
-const Header = ({ course }) => {
-  return <h2>{course}</h2>;
-};
-
-const Part = ({ part, exercise }) => {
-  return (
-    <p>
-      {part} {exercise}
-    </p>
-  );
-};
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map((part) => (
-        <Part key={part.id} part={part.name} exercise={part.exercises} />
-      ))}
-    </div>
-  );
-};
-
-const Total = ({ parts }) => {
-  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
-  return (
-    <p>
-      <b>Total of {total} exercises</b>
-    </p>
-  );
-};
-
-const Course = ({ course }) => {
-  return (
-    <>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </>
-  );
-};
+import Course from "./components/Course";
 
 const App = () => {
   const courses = [
