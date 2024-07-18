0a0657b73a4d5be832b0f44513bdf5cdb7946b12
Kaukiainen, Sergei <sergei.kaukinen@roimaint.com>
2024-07-18 16:18:03 +0300

    exercise 3.7-3.8 puhelinluettelon backend step7-step8

--- osa3/index.js
+++ osa3/index.js
@@ -1,7 +1,25 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
 app.use(express.json());
+
+morgan.token("body", (req) => JSON.stringify(req.body));
+
+app.use(
+  morgan("tiny", {
+    skip: (res) => res.method === "POST",
+  })
+);
+
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :body",
+    {
+      skip: (res) => res.method !== "POST",
+    }
+  )
+);
 
 let persons = [
   {
